dba1c9374349c8d8c5eea843c20c15bc78ad4a4b
Mark Ferianec <134004771+MarkFerianec@users.noreply.github.com>
2023-11-28 21:10:53 -0700

    Create various files
    Created various necessary files for beginning of project.

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('this is a test');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLDhCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vb2Rpbi10b2RvLWxpc3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ3RoaXMgaXMgYSB0ZXN0Jyk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
