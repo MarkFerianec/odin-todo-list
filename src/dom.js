716bc27072651eca236baee31a41fdc00b800609
Mark Ferianec <134004771+MarkFerianec@users.noreply.github.com>
2023-12-04 20:10:00 -0700

    Modify dom
    Added a function that clears New Project form after submit. Deleted
    comments.

--- src/dom.js
+++ src/dom.js
@@ -143,7 +143,7 @@ function displayProjects() {
             deleteToDoItemButton.addEventListener('click', deleteToDoItemFunction);
 
             function deleteToDoItemFunction() {
-                console.log(projects[i].toDoItems[a].title);
+                // console.log(projects[i].toDoItems[a].title);
                 // alert(projects[i].toDoItems[a].title);
                 // alert(projects[i].projectTitle);
                 // projects[i].projectTitle.removeChild(projects[i].toDoItems[a].title);
@@ -169,8 +169,7 @@ function displayProjects() {
 
 const newProjectButtonQuerySelector = document.querySelector('#submit');
 
-newProjectButtonQuerySelector.addEventListener('click', addNewProject);
-
+newProjectButtonQuerySelector.addEventListener('click', addNewProjectAndClearNewProjectForm);
 
 function addNewProject() {
 
@@ -178,7 +177,7 @@ function addNewProject() {
 
     const newProjectDiv = document.createElement('div');
 
-    let titleOfNewProject = document.querySelector('#project-title');
+    const titleOfNewProject = document.querySelector('#project-title');
 
     newProjectDiv.classList.add(titleOfNewProject.value);
 
@@ -265,196 +264,17 @@ function addNewProject() {
 
 }
 
+function clearNewProjectForm() {
+    const titleOfNewProject = document.querySelector('#project-title');
+    titleOfNewProject.value = '';
+}
+
+function addNewProjectAndClearNewProjectForm() {
+    addNewProject();
+    clearNewProjectForm();
+}
+
 // export { displayProjects };
 // export { displayProjects, addProject };
 export { displayProjects, addNewProject };
 
-
-
-
-
-
-
-// Somethis like this I believe is correct:
-// for (let y = 0; y < projects.length; y++) {
-//     // console.log(projects[y].toDoItems[y].title);
-//     // console.log(projects[y]);
-//     // projects[y]
-//     // console.log(projects[y].toDoItems);
-
-//     for (let a = 0; a < projects[y].toDoItems.length; a++) {
-//         // console.log(a);
-//         console.log(projects[y].toDoItems[a].title);
-//     }
-// }
-
-
-
-
-
-
-//Just saving this here incase I need to go back:
-// for (let i = 0; i < projects.length; i++) {
-//     // console.log("test");
-//     const div = document.createElement("div");
-//     projectsDiv.appendChild(div);
-//     div.classList.add(projects[i].projectTitle);
-//     div.textContent = projects[i].projectTitle;
-
-//     // const div2 = document.createElement("div");
-//     // div.appendChild(div2);
-//     // div2.classList.add(projects[0].toDoItems[0].title);
-//     // div2.textContent = projects[0].toDoItems[0].title; 
-// }
-
-
-
-
-
-
-
-
-
-// function displayProjects() {
-
-//     const content = document.querySelector(".content");
-
-//     const projectsDiv = document.createElement("div");
-//     projectsDiv.classList.add("projects");
-
-//     content.appendChild(projectsDiv);
-
-//     for (let i = 0; i < projects.length; i++) {
-//         // console.log("test");
-//         const div = document.createElement("div");
-//         projectsDiv.appendChild(div);
-//         div.classList.add(projects[i].projectTitle);
-//         div.textContent = projects[i].projectTitle;
-//     }
-
-// }
-
-
-
-
-
-
-
-// function displayProjects() {
-
-//     const content = document.querySelector(".content");
-
-//     const projectsDiv = document.createElement("div");
-//     projectsDiv.classList.add("projects");
-
-//     content.appendChild(projectsDiv);
-
-//     for (let i = 0; i < projects.length; i++) {
-//         // console.log("test");
-//         const div = document.createElement("div");
-//         projectsDiv.appendChild(div);
-//         div.classList.add(projects[i].projectTitle);
-//         div.textContent = projects[i].projectTitle;
-
-//         // const div2 = document.createElement("div");
-//         // div.appendChild(div2);
-//         // div2.classList.add(projects[0].toDoItems[0].title);
-//         // div2.textContent = projects[0].toDoItems[0].title;
-
-//         for (let j = 0; j < projects.length; j++) {
-//             const div2 = document.createElement("div");
-//             div.appendChild(div2);
-//             // div2.classList.add(projects[i])
-//             console.log(j);
-//         }
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-// console.log(projects[0].toDoItems.length);
-
-// for (let j = 0; j < projects[0].toDoItems.length; j++) {
-//     console.log(projects[0].toDoItems[j].title);
-// }
-
-// for (let k = 0; k < projects[1].toDoItems.length; k++) {
-//     console.log(projects[1].toDoItems[k].title);
-// }
-
-// important:
-// for (let y = 0; y < projects.length; y++) {
-//     // console.log(projects[y].toDoItems[y].title);
-//     // console.log(projects[y]);
-//     // projects[y]
-//     // console.log(projects[y].toDoItems);
-
-//     for (let a = 0; a < projects[y].toDoItems.length; a++) {
-//         // console.log(a);
-//         console.log(projects[y].toDoItems[a].title);
-//     }
-// }
-
-// for (let y = 0; y < projects.length; y++) {
-//     console.log(projects[y].toDoItems[y].title);
-// }
-
-
-
-
-
-
-
-
-
-// function addProject() {
-
-//     const content = document.querySelector(".content");
-
-//     const dialogDiv = document.createElement('div');
-//     dialogDiv.classList.add("dialog");
-
-//     content.appendChild(dialogDiv);
-
-//     const projectForm = document.createElement('form');
-//     projectForm.classList.add('form');
-
-//     dialogDiv.appendChild(projectForm);
-
-//     let inputProjectName = document.createElement('input');
-//     inputProjectName.type = 'text';
-//     inputProjectName.name = 'project-name';
-//     // inputProjectName.value = 'test';
-
-//     let inputProjectDescription = document.createElement('input');
-//     inputProjectDescription.type = 'text';
-//     inputProjectDescription.name = 'project-description';
-
-//     let inputProjectDueDate = document.createElement('input');
-//     inputProjectDueDate.type = 'text';
-//     inputProjectDueDate.name = 'due-date';
-
-//     let inputProjectPriority = document.createElement('input');
-//     inputProjectPriority.type = 'text';
-//     inputProjectPriority.name = 'due-date';
-
-//     let projectSubmitButton = document.createElement('input');
-//     projectSubmitButton.type = 'submit';
-//     projectSubmitButton.value = 'submit';
-
-//     projectForm.appendChild(inputProjectName);
-//     projectForm.appendChild(inputProjectDescription);
-//     projectForm.appendChild(inputProjectDueDate);
-//     projectForm.appendChild(inputProjectPriority);
-//     projectForm.appendChild(projectSubmitButton);
-//     // const projectTitle = document.createElement('label');
-
-// }
